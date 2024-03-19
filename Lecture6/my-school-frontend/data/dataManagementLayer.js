d147d09955303f93b5e99e756880b4f577f4b348
Evgeny Leonov <leonov.eugene@gmail.com>
2024-03-19 10:48:21 +0100

    Update dataManagementLayer.js

--- Lecture6/my-school-frontend/data/dataManagementLayer.js
+++ Lecture6/my-school-frontend/data/dataManagementLayer.js
@@ -1,2 +1,17 @@
 const baseUrl = "http://localhost:3001/";
 
+async function fetchRoutines(url) {
+    const urlFull = baseUrl + url;
+    console.log("requesting: " + urlFull);
+    let result = null;
+    await fetch(urlFull)
+        .then((res) => res.json())
+        .then((parsedJson) => result = parsedJson);
+    console.log("result= " + JSON.stringify(result));
+    return result;
+}
+
+async function readClasses() {
+    const data = await fetchRoutines("classes/listWithDetails");
+    return data.items;
+}
